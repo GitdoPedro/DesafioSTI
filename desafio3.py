047c22bb41d9cfc0ab7bda16b02f03469ffd5b16
Pedro Lima <64121805+GitdoPedro@users.noreply.github.com>
2020-12-02 19:44:44 -0300

    ultimo commit
    tentativa de implementar a média dos cr's dos cursos

--- desafio3.py
+++ desafio3.py
@@ -1,39 +1,51 @@
 import sys
 import csv
 
+
 def lerArquivo(arq):  ## Lê o arquivo csv com as notas
     with open(arq, "r") as notas_csv:
         dados = []
         materias = csv.DictReader(notas_csv, delimiter=",")
         for materia in materias:
-            dados.append(materia)
+             dados.append(materia)
     return dados
 
 class CalculoDoCR(object):
     def __init__(self, notas): 
         self.notas = notas
-        self.calculaCr(notas)
         
-
-    def calculaCr(self, nota):  ## efetua o calculo e imprime os cr's e as matriculas
+       
+        print("------- O CR dos alunos é: --------")
+        self.calculaCr(notas,"MATRICULA")
+        
+        
+        """
+        print("----- Média de CR dos cursos ------")
+        self.calculaCr(notas,"COD_CURSO")
+        """
+        
+    def calculaCr(self, nota,coluna):  ## efetua o calculo e imprime os cr's e as matriculas
         notaCargaHoraria = 0
         cargaHorariaTotal = 0
-        matricula = nota[0]["MATRICULA"]
-        print("------- O CR dos alunos é: --------")
+        metodo = nota[0][coluna]
+        ##crCalculado = {}
         for item in range(len(nota)):
+            
                     
-            if matricula == nota[item]["MATRICULA"]:
+            if metodo == nota[item][coluna]:
                 
                 notaCargaHoraria += int(nota[item]["CARGA_HORARIA"])* int(nota[item]["NOTA"]) 
                 cargaHorariaTotal += int(nota[item]["CARGA_HORARIA"])
             else:
-               
-                print(matricula, " -  %.1f" %(float(notaCargaHoraria / cargaHorariaTotal)))
-                matricula = nota[item]["MATRICULA"]
+
+                if cargaHorariaTotal>0:
+                    print(metodo, " -  %.1f" %(float(notaCargaHoraria / cargaHorariaTotal)))                      
+                metodo = nota[item][coluna]
                 notaCargaHoraria = 0
                 cargaHorariaTotal = 0
      
-        print(matricula, " -  %.1f" %(float(notaCargaHoraria / cargaHorariaTotal)))
+        print(metodo, " -  %.1f" %(float(notaCargaHoraria / cargaHorariaTotal)))
+        print("-----------------------------------")
         
 
 def main():
